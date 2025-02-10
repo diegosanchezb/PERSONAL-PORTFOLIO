a494ac685f9dbc23877841609108f56fa69e04e3
Diego Sánchez <sanchezbrisadiego@gmail.com>
2025-02-10 18:02:12 -0300

    Actualizar favicon y remover enlace de Twitter

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -28,14 +28,6 @@ export const Footer = () => {
             <FaInstagram size={26} />
           </a>
           <a
-            href="https://x.com/diesanchezb"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-lime-800 hover:text-lime-600 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4) transition"
-          >
-            <FaTwitter size={26} />
-          </a>
-          <a
             href="https://wa.me/542284522717"
             target="_blank"
             rel="noopener noreferrer"
